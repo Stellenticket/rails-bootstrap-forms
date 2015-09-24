cfc88e4b7baddfe3cff58d857f103ba762ad254f
Markus Doits <markus.doits@stellenticket.de>
2015-09-24 12:35:03 +0200

    Add required class to form-group, too

--- lib/bootstrap_form/form_builder.rb
+++ lib/bootstrap_form/form_builder.rb
@@ -188,6 +188,7 @@ module BootstrapForm
       options[:class] = ["form-group", options[:class]].compact.join(' ')
       options[:class] << " #{error_class}" if has_error?(name)
       options[:class] << " #{feedback_class}" if options[:icon]
+      options[:class] << " required" if required_attribute?(object, name)
 
       content_tag(:div, options.except(:id, :label, :help, :icon, :label_col, :control_col, :layout)) do
         label = generate_label(options[:id], name, options[:label], options[:label_col], options[:layout]) if options[:label]
